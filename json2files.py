c2693412124f8f95c0cd2b6b87765ed5e92b4cc6
maxgoldhouse <dklongley@maxgoldhouse.com>
2022-03-09 12:49:31 +0000

    [skip ci] add json2files py

---
+++ json2files.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+import json
+import os
+import sys
+
+jsondict = {}
+
+with open('feeds.json') as feedfile:
+  data = json.load(feedfile)
+
+for feed in data['feeds']:
+   contentlocation = '/workspace/content/'+feed['folder']+'/'
+   print('cont loc: '+contentlocation)
+   url = feed['url']
+   response = urlopen(feed['url'])  
+   jsondict = json.loads(response.read())
+
+   for key in jsondict:
+     for lock in key:
+        #-## print(key[lock])
+        filename = contentlocation+key[lock]['ref']+'.md'
+        print('filename: '+filename)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'w') as f:
+          print('prop ref: '+key[lock]['ref'])
+          f.write(json.dumps(key[lock], indent = 0)) 
