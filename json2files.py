29b76abd2c08b684f3864f342af757aa7d306b63
dindinet <2939526+dindinet@users.noreply.github.com>
2022-03-21 17:41:46 +0000

    getting ready to push to firebase

--- json2files.py
+++ json2files.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
 import json
 import os
 import sys
@@ -24,3 +24,13 @@ for feed in data['feeds']:
         with open(filename, 'w') as f:
           print('prop ref: '+key[lock]['ref'])
           f.write(json.dumps(key[lock], indent = 0)) 
+
+partialsfolder = "/workspace/layouts/partials/"
+## fetch and save  alllocations
+groupedbytownurl = 'https://mgh-props.appspot.com/getgroupedbytown'
+locationsurl = "http://mgh-props.appspot.com/locations"
+urlretrieve (groupedbytownurl,partialsfolder+"alllocations.html")
+
+## fetch and save proprefs
+proprefsurl = 'https://mgh-props.appspot.com/refs'
+urlretrieve (proprefsurl, partialsfolder+"refs.html")
